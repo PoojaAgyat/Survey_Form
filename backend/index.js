d14d9b3b3404cacc599b006097012a29c87547ef
Imran31051998 <imran31051998@gmail.com>
2023-04-23 11:29:01 +0530

    modified all

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,87 @@
-const express = require('express')
-const bodyParser = require("body-parser");
-const cors = require("cors")
-const router = require('./routes/user');
+const express = require("express");
+const port = 8080 || process.env.PORT;
+//const mongoose = require("mongoose");
+require('dotenv').config()
+const profile = require("./models/profile")
+const theme = require("./models/theme")
+const cors = require("cors");
 const connection = require("./connection/connection");
-const port = 3030 || process.env.PORT
-const app = express()
+const datas = require("./data")
+const jwt = require("jsonwebtoken")
+const Questions = require("./routes/question")
+const UserRoutes = require("./routes/user")
+const secret = "MAINPROJECT"
+
+const app = express();
+app.use(express.json());
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 connection()
   .then(() => {
-    app.listen(port, () => console.log("Server is running at port 3030...",port));
+    app.listen(port, () => console.log("Server is running at port 8080...",port));
   })
   .catch(() => {
     console.log("Failed to connect to database!");
   });
-  app.use("/", router);
+
+app.use("/createForm", (req, res, next) => {
+  const token = req.headers.authorization;
+  if(token){
+    
+      jwt.verify(token, secret, function(err, decoded) {
+          if(err) {
+              console.log(err);
+             return res.status(403).json({
+              status: "Failed", 
+              message: "Token is not valid"
+              });
+          }
+          req.user = decoded.data;
+          next();
+        });
+
+  }else {
+      res.status(403).json({
+          status: "Failed", 
+          message: "User is not authenticated"
+      })
+  }
+})
+
+app.get('/api/themes',async  (req, res) => {
+ //  theme.deleteMany(data.data)
+//console.log(datas)
+  theme.insertMany(datas.data)
+const data = await theme.find()
+    res.json({
+        data 
+    }
+    ); 
+  });
+
+app.post("/profile" , async (req,res) => {
+  console.log(req.body)
+  const {image} = req.body
+  await profile.create({
+    image:image
+  })
+})
+ app.get("/profile" , async(req,res) => {
+  const image = await profile.find();
+  //console.log(image)
+  res.json({
+    image:image
+  })
+ })
  
+app.use("/createForm" , Questions)
+app.use("/user" , UserRoutes)
+
+
+/*mongoose.connect(
+   process.env.MONGODB_URL  ,
+   
+).then(() => console.log("connected to db"))
+
+
+app.listen(PORT , () => {console.log(`server is up and running at port number ${PORT}`)})  */
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,167 +1,153 @@
-const express = require("express")
-const Login = require("../models/User");
-const surveyModel = require("../models/surveyModel")
-const questionModel=require("../models/questionModel")
-const Theme = require('../models/Theme')
+const express = require("express");
+const { body, validationResult } = require('express-validator');
+const User = require("../models/user");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload")
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt");
-const route = express.Router()
-const SC_KEY = "MUKKOTI&&SHASHANK&&123456789"
-module.exports = route
-route.post("/login", async (req, res) => {
-    try {
-        const Registereduser = await Login.findOne({ email: req.body.email })
-        if (!Registereduser) {
-            res.status(400).json({
-                status: "error",
-                message: "please Register to get login"
-            })
-        } else {
-            bcrypt.compare(req.body.password, Registereduser.password, async function (err, result) {
-                if (err) {
-                    res.status(400).json({
-                        status: "error",
-                        message: err
+const secret = "MAINPROJECT";
+
+const router = express.Router();
+mongoose.set("strictQuery", true);
+router.use(cors());
+router.use(fileUpload())
+router.use(express.json());
+ 
+ 
+router.post("/register",
+    body("name").isAlpha().isLength({min:5}), 
+    body('email').isEmail(),
+    body('password').isLength({ min: 6, max: 12 }),
+    body("phone").isNumeric({min:10 , max:10})
+    , async (req, res) => {
+console.log(req.body)
+        try {
+            console.log(req.body)
+            const errors = validationResult(req);
+            console.log(errors)
+            if (!errors.isEmpty()) {
+                const error = errors.array()[0];
+                if (error.param == "name") {
+                    return res.status(400).json({
+                        message : "Invalid Name"
+                    });
+                } else if (error.param == "email"){
+                    return res.status(400).json({
+                        message : "Invalid Email"
+                    });
+                }else if(error.param == "phone"){
+                    return res.status(400).json({
+                        message:"Invalid Phone Number"
                     })
-                } else {
-                    if (result) {
-                        const data = await Login.findOne({ email: req.body.email })
-                        const token = jwt.sign({
-                            data: data._id
-                        }, SC_KEY, { expiresIn: 90000000 });
-                        res.status(200).json({
-                            status: "sucess",
-                            message: "user successfully verified ",
-                            token
-                        })
-                    } else {
-                        res.status(400).json({
-                            status: "error",
-                            message: "incorrect password"
-                        })
-
-                    }
                 }
-            });
-        }
-
-    } catch {
-
-    }
-})
-route.post("/register", async (req, res) => {
-    try {
-
-        const Registereduser = await Login.findOne({ email: req.body.email })
-        //checking user is existed or not
-        if (!Registereduser) {
-            try {
-
-                bcrypt.hash(req.body.password, 10, async function (err, hash) {
-                    if (err) {
-                        res.status(400).json({
-                            status: "error",
-                            message: err
-                        })
-                    } else {
-                        await Login.create({ name: req.body.name, email: req.body.email, phone: req.body.phone, password: hash, profession: req.body.profession })
-                        res.status(200).json({
-                            status: "sucess",
-                            message: "sucessfully registered, please login"
-
-                        })
+                else if(password != confirmpassword){
+                    return res.json.status(400).json({
+                        message:"Password and Confirm Password should be same"
+                    })
                     }
-                });
-
+                
+                 else {
+                    return res.status(400).json({
+                      message: "Invalid Password"
+                    });
+                }
             }
-            catch (err) {
-                res.status(400).json({
-                    status: "error",
-                    message: err.message
+         
+            const { name, email, password,phone,profession,confirmpassword } = req.body;
+            console.log(req.body)
+            const user = await User.findOne({ email });
+            if (user) {
+                return res.status(409).json({
+                    status: "Failed",  
+                    message: "User already exists"
+                });
+            }
+        
+            bcrypt.hash(password, 10, async function (err, hash) {
+           
+                if (err) {
+                    return res.status(500).json({ 
+                        status: "Failed",
+                        message: err.message
+                    })
+                }
+                await User.create({
+                    name,
+                    email,
+                    password: hash,
+                    phone,
+                    profession,
+                    confirmpassword
+                });
+                return res.status(200).json({
+                    status: "Success",
+                    message : "User registered successfully"
+                })
+            });
+        } catch (e) {
+          
+            return res.status(500).json({
+                status: "Failed",
+                message:e.message
+            })
+        }
+    });
+
+
+router.post("/login", 
+    body('email').isEmail(), async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                  errors: errors.array(),
+                  message: "Invalid Email",
+                });
+            }
+            console.log(req.body)
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({
+                    status: "Failed",
+                    message: "User not registered, please register"
                 })
             }
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (err) {
+                    return res.status(500).json({
+                        status: "Failed",
+                        message: err.message
+                    })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                        data: user._id,
+                    }, secret);
 
-        } else {
-            res.status(400).json({
-                status: "error",
-                message: "user already registered, you can login directly"
+                    return res.status(200).json({
+                        status: "Succces",
+                        message : "Login successfully",
+                        token
+                    })
+                }
+                 else {
+                    return res.status(400).json({
+                        status: "Failed",
+                        message: "Wrong Password"
+                    })
+                }
+            });
+
+
+        } catch (e) {
+            return res.status(500).json({
+                status: "Failed",
+                message: "e.message"
             })
         }
-
-    }
-    catch (err) {
-
-        res.status(400).json({
-            status: "error",
-            message: err.message
-        })
-
-    }
-
-})
-route.post("/createsurvey", async (req, res) => {
-    const { name, startDate, endDate, description, typeOfSurvey, criteria, img } =
-        req.body;
-    try {
-        const survey = await surveyModel.create({
-            name: name,
-            startDate: startDate,
-            endDate: endDate,
-            description: description,
-            typeOfSurvey: typeOfSurvey,
-            criteria: c+riteria,
-            image: img,
-        });
-        res.status(200).json({
-            status: "Success",
-            data: survey,
-            message: "Data Save Successfully!",
-        });
-    } catch (error) {
-        res.status(500).json({ error });
-    }
-})
-route.get("/surveylist", async (req, res) => {
-    try {
-        const list = await surveyModel.find({});
-
-        res.status(200).json(list);
-    } catch (error) {
-        res.status(500).json({ Error: error });
-    }
-});
-route.post("/que", async (req, res) => {
-    //console.log(req.body);
-    const questions = req.body.questions;
-    const option = req.body.option;
-    const response = req.body.answers;
-  
-    const data = await questionModel.create({
-      questions: questions,
-      option: option,
-      response: response,
     });
-    res.json({
-      status: "successful",
-      message: "questions added",
-    });
-  });
-  
-  route.get("/getque", async (req, res) => {
-    const query = req.body.id;
-    console.log(query);
-    const questions = await questionModel.findOne({ question_id: query });
-    console.log(questions);
-    res.json({
-      questions: questions,
-    });
-  });
-  route.post("/createheme", async (req, res) => {
-    try {
-      const theme = await Theme.create(req.body)
-      res.status(201).json({ theme })
-    }
-    catch (err) {
-      console.log(err)
-    }
-  });
+module.exports = router;
